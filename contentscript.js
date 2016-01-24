c8133e1b16ef4961f22b3c320410b7f538f18ce4
kaptainkommie@users.noreply.github.com <kaptainkommie@users.noreply.github.com>
2016-01-24 00:04:56 -0500

    bugfixes
    Some minor bugfixes spread across the files. Category and sort options
    should now have effect on the search button url as intended. Closes
    issue #3

--- contentscript.js
+++ contentscript.js
@@ -20,49 +20,182 @@
 //	Email: rohn.adams@gmail.com
  */
 
-//Here comes the kludge; not particuarly well-versed in JS or DOM
-//pull the item title, if IMDb layout changes, this might need to as well
-var itemTitle = document.querySelector("span.itemprop[itemprop=name]").innerHTML;
+var DEFAULT_OPTIONS = { // options to use if loadOptions fails
+    "all_movies": true,
+    "movie_3d": true,
+    "movie_480p": true,
+    "movie_bd-r": true,
+    "movie_bd-rip": true,
+    "movie_cam": true,
+    "movie_dvd-r": true,
+    "movie_hd-bluray": true,
+    "movie_kids": true,
+    "movie_mp4": true,
+    "movie_non-english": true,
+    "movie_packs": true,
+    "movie_web-dl": true,
+    "movie_xvid": true,
 
-//get item year
-var itemYear = document.querySelector("span.nobr a").innerHTML;
+    "all_tv": true,
+    "tv_documentaries": true,
+    "tv_sports": true,
+    "tv_480p": true,
+    "tv_bd": true,
+    "tv_dvd-r": true,
+    "tv_dvd-rip": true,
+    "tv_mp4": true,
+    "tv_non-english": true,
+    "tv_packs": true,
+    "tv_packs-non-english": true,
+    "tv_sd-x264": true,
+    "tv_web-dl": true,
+    "tv_x264": true,
+    "tv_xvid": true,
 
-//get location where button will be placed
-//TODO: spacing is off, have to try some things
-var buttonWrapper = document.querySelector("span.wlb_wrapper");
+    "sort_options": "newest"
+}
 
-var iptSearchLink = createIPTSearchLink(itemTitle, itemYear);
+var OPTION_VALUES = { // values associated with category
+    "all_movies": 72,
+    "movie_3d": 87,
+    "movie_480p": 77,
+    "movie_bd-r": 89,
+    "movie_bd-rip": 90,
+    "movie_cam": 96,
+    "movie_dvd-r": 6,
+    "movie_hd-bluray": 48,
+    "movie_kids": 54,
+    "movie_mp4": 62,
+    "movie_non-english": 38,
+    "movie_packs": 68,
+    "movie_web-dl": 20,
+    "movie_xvid": 7,
 
-//TODO: there's got to be a better way, even without jQuery
-//create the appropriate anchor (a href)
-var searchAnchor = document.createElement("a");
+    "all_tv": 73,
+    "tv_documentaries": 26,
+    "tv_sports": 55,
+    "tv_480p": 78,
+    "tv_bd": 23,
+    "tv_dvd-r": 24,
+    "tv_dvd-rip": 25,
+    "tv_mp4": 66,
+    "tv_non-english": 82,
+    "tv_packs": 65,
+    "tv_packs-non-english": 83,
+    "tv_sd-x264": 79,
+    "tv_web-dl": 22,
+    "tv_x264": 5,
+    "tv_xvid": 4,    
+}
 
-searchAnchor.setAttribute("href", iptSearchLink, isTVSeries());
-searchAnchor.className = "btn2 btn2_text_on large";
+//similar function is present in options.js as well, but you can't include
+//a js file inside another pure JS file, oh well
+function loadOptions(optionsObject, defaultOptions, optionValues) {  
+  chrome.storage.sync.get(null, function(items) {
+    if(!chrome.runtime.lastError) {
+      if(Object.getOwnPropertyNames(items).length > 0)
+      {
+        for(var prop in items) {
+          optionsObject[prop] = items[prop];          
+        }
+      }
+      else {
+        console.log(chrome.runtime.lastError);
+        for(var prop in defaultOptions) {
+          optionsObject[prop] = defaultOptions[prop];
+        }
+      }
 
-//span for the button text itself
-var searchSpan = document.createElement("span");
-searchSpan.className = "btn2_text";
-searchSpan.textContent = "Search IPT";
+      addSearchButton(optionsObject, optionValues);
+    }
+  })
+}
 
-//add to the DOM
-searchAnchor.appendChild(searchSpan);
-buttonWrapper.appendChild(searchAnchor);
+function addSearchButton(opt, optValues) {
+  //Here comes the kludge; not particuarly well-versed in JS or DOM
+  //pull the item title, if IMDb layout changes, this might need to as well
+  var itemTitle = document.querySelector("span.itemprop[itemprop=name]").innerHTML;
+
+  //get item year
+  var itemYear = document.querySelector("span.nobr a").innerHTML;
+
+  //get location where button will be placed
+  //TODO: spacing is off, have to try some things
+  var buttonWrapper = document.querySelector("span.wlb_wrapper");
+
+  var iptSearchLink = createIPTSearchLink(itemTitle, itemYear, opt, optValues);
+
+  //TODO: there's got to be a better way, even without jQuery
+  //create the appropriate anchor (a href)
+  var searchAnchor = document.createElement("a");
+
+  searchAnchor.setAttribute("href", iptSearchLink);
+  searchAnchor.className = "btn2 btn2_text_on large";
+
+  //span for the button text itself
+  var searchSpan = document.createElement("span");
+  searchSpan.className = "btn2_text";
+  searchSpan.textContent = "Search IPT";
+
+  //add to the DOM
+  searchAnchor.appendChild(searchSpan);
+  buttonWrapper.appendChild(searchAnchor);
+}
+
+var options = new Object();
+
+loadOptions(options, DEFAULT_OPTIONS, OPTION_VALUES);
 
 //create the IPT search link
 //return escaped IPT search link for particular title
-//TODO: punctuation general doesn't return good results, option to strip?
-function createIPTSearchLink(title, year, tvshow) {
-  // different base depending on title type
+//TODO: punctuation generally doesn't return good results, option to strip?
+function createIPTSearchLink(title, year, optionsObject, optionValues) {
   // TODO: implement categories from extension options
-  if (isTVSeries)
-    linkBase = "https://www.iptorrents.com/t?73=&q=";
-  else
-    linkBase = "https://www.iptorrents.com/t?72=&q=";
+  link = "https://www.iptorrents.com/t?"
+
+    // different base depending on title type and options
+    if (isTVSeries()) {
+      // only care about tv options
+      // if all_tv selected, we don't need to loop
+      if(optionsObject["all_tv"] == true)
+        link += optionValues["all_tv"] + ";";
+      else {
+        for(var prop in optionsObject) {
+          // example multicategory url:
+          // https://www.iptorrents.com/t?72;o=size;q=V%20for%20Vendetta#torrents
+          // only add category if it is enabled
+          if(optionsObject.hasOwnProperty(prop)) {
+            if(prop.indexOf("tv_") != -1 && 
+                optionsObject[prop] == true) {
+              link += optionValues[prop] + ";";
+            } 
+          }
+        }
+      }
+    }
+    else {
+      if(optionsObject["all_movies"] == true)
+        link += optionValues["all_movies"] + ";";     
+      else {
+        for(var prop in optionsObject) {
+          // only add category if it is enabled
+          if(optionsObject.hasOwnProperty(prop)) {
+            if(prop.indexOf("movie_") != -1 && 
+                optionsObject[prop] == true) {
+              link += optionValues[prop] + ";";
+            }
+          }
+        }
+      }
+    }
+  
+  // add sort option
+  if(optionsObject.hasOwnProperty("sort_options"))
+    link += "o=" + optionsObject["sort_options"] + ";";
 
   // TODO: test that escape works on weird chars in title
-  // doesn't encode spaces, but chrome seems to automatically do that
-  fullLink = linkBase + escape(title) + "&qf=#torrents"; 
+  // doesn't encode spaces, but chrome seems not to care
+  fullLink = link + "q=" + escape(title) + "#torrents"; 
 
   return fullLink;
 }
--- options.js
+++ options.js
@@ -1,36 +1,36 @@
 var DEFAULT_OPTIONS = { // options to use if storage.sync fails or doesn't exist
-  "all_movies": true,
-  "movie_3d": true,
-  "movie_480p": true,
-  "movie_bd-r": true,
-  "movie_bd-rip": true,
-  "movie_cam": true,
-  "movie_dvd-r": true,
-  "movie_hd-bluray": true,
-  "movie_kids": true,
-  "movie_mp4": true,
-  "movie_non-english": true,
-  "movie_packs": true,
-  "movie_web-dl": true,
-  "movie_xvid": true,
+    "all_movies": true,
+    "movie_3d": true,
+    "movie_480p": true,
+    "movie_bd-r": true,
+    "movie_bd-rip": true,
+    "movie_cam": true,
+    "movie_dvd-r": true,
+    "movie_hd-bluray": true,
+    "movie_kids": true,
+    "movie_mp4": true,
+    "movie_non-english": true,
+    "movie_packs": true,
+    "movie_web-dl": true,
+    "movie_xvid": true,
 
-  "all_tv": true,
-  "tv_documentaries": true,
-  "tv_sports": true,
-  "tv_480p": true,
-  "tv_bd": true,
-  "tv_dvd-r": true,
-  "tv_dvd-rip": true,
-  "tv_mp4": true,
-  "tv_non-english": true,
-  "tv_packs": true,
-  "tv_packs-non-english": true,
-  "tv_sd-x264": true,
-  "tv_web-dl": true,
-  "tv_x264": true,
-  "tv_xvid": true,
-  
-  "sort_options": "newest"
+    "all_tv": true,
+    "tv_documentaries": true,
+    "tv_sports": true,
+    "tv_480p": true,
+    "tv_bd": true,
+    "tv_dvd-r": true,
+    "tv_dvd-rip": true,
+    "tv_mp4": true,
+    "tv_non-english": true,
+    "tv_packs": true,
+    "tv_packs-non-english": true,
+    "tv_sd-x264": true,
+    "tv_web-dl": true,
+    "tv_x264": true,
+    "tv_xvid": true,
+
+    "sort_options": "newest"
 }
 
 var optionsElements = new Object(); // hold DOM "options" elements
@@ -39,40 +39,40 @@ var currentOptions = new Object(); // hold the actual options
 
 function getOptionsElements(elements) {
   options =  {
-    "all_movies": document.getElementById("all_movies"),
-    "movie_3d": document.getElementById("movie_3d"),
-    "movie_480p": document.getElementById("movie_480p"),
-    "movie_bd-r": document.getElementById("movie_bd-r"),
-    "movie_bd-rip": document.getElementById("movie_bd-rip"),
-    "movie_cam": document.getElementById("movie_cam"),
-    "movie_dvd-r": document.getElementById("movie_dvd-r"),
-    "movie_hd-bluray": document.getElementById("movie_hd-bluray"),
-    "movie_kids": document.getElementById("movie_kids"),
-    "movie_mp4": document.getElementById("movie_mp4"),
-    "movie_non-english": document.getElementById("movie_non-english"),
-    "movie_packs": document.getElementById("movie_packs"),
-    "movie_web-dl": document.getElementById("movie_web-dl"),
-    "movie_xvid": document.getElementById("movie_xvid"),
-    
-    "all_tv": document.getElementById("all_tv"),
-    "tv_documentaries": document.getElementById("tv_documentaries"),
-    "tv_sports": document.getElementById("tv_sports"),
-    "tv_480p": document.getElementById("tv_480p"),
-    "tv_bd": document.getElementById("tv_bd"),
-    "tv_dvd-r": document.getElementById("tv_dvd-r"),
-    "tv_dvd-rip": document.getElementById("tv_dvd-rip"),
-    "tv_mp4": document.getElementById("tv_mp4"),
-    "tv_non-english": document.getElementById("tv_non-english"),
-    "tv_packs": document.getElementById("tv_packs"),
-    "tv_packs-non-english": document.getElementById("tv_packs-non-english"),
-    "tv_sd-x264": document.getElementById("tv_sd-x264"),
-    "tv_web-dl": document.getElementById("tv_web-dl"),
-    "tv_x264": document.getElementById("tv_x264"),
-    "tv_xvid": document.getElementById("tv_xvid"),
-    
-    "sort_options": document.getElementById("sort_options")
+      "all_movies": document.getElementById("all_movies"),
+      "movie_3d": document.getElementById("movie_3d"),
+      "movie_480p": document.getElementById("movie_480p"),
+      "movie_bd-r": document.getElementById("movie_bd-r"),
+      "movie_bd-rip": document.getElementById("movie_bd-rip"),
+      "movie_cam": document.getElementById("movie_cam"),
+      "movie_dvd-r": document.getElementById("movie_dvd-r"),
+      "movie_hd-bluray": document.getElementById("movie_hd-bluray"),
+      "movie_kids": document.getElementById("movie_kids"),
+      "movie_mp4": document.getElementById("movie_mp4"),
+      "movie_non-english": document.getElementById("movie_non-english"),
+      "movie_packs": document.getElementById("movie_packs"),
+      "movie_web-dl": document.getElementById("movie_web-dl"),
+      "movie_xvid": document.getElementById("movie_xvid"),
+
+      "all_tv": document.getElementById("all_tv"),
+      "tv_documentaries": document.getElementById("tv_documentaries"),
+      "tv_sports": document.getElementById("tv_sports"),
+      "tv_480p": document.getElementById("tv_480p"),
+      "tv_bd": document.getElementById("tv_bd"),
+      "tv_dvd-r": document.getElementById("tv_dvd-r"),
+      "tv_dvd-rip": document.getElementById("tv_dvd-rip"),
+      "tv_mp4": document.getElementById("tv_mp4"),
+      "tv_non-english": document.getElementById("tv_non-english"),
+      "tv_packs": document.getElementById("tv_packs"),
+      "tv_packs-non-english": document.getElementById("tv_packs-non-english"),
+      "tv_sd-x264": document.getElementById("tv_sd-x264"),
+      "tv_web-dl": document.getElementById("tv_web-dl"),
+      "tv_x264": document.getElementById("tv_x264"),
+      "tv_xvid": document.getElementById("tv_xvid"),
+
+      "sort_options": document.getElementById("sort_options")
   }
-  
+
   // copy options properties, to elements object
   for (var prop in options) {
     elements[prop] = options[prop]
@@ -105,12 +105,12 @@ function writeOptionsPage(elements, optionsObject) {
 function initializeOptions(elements, optionsObject, defaultOptions) {
   getOptionsElements(elements);
   loadOptions(elements, optionsObject, defaultOptions);
-  
+
   document.getElementById("movie_categories").addEventListener("change", 
       function(event) {
     onCategoryChange(elements, event);
   });
-  
+
   document.getElementById("tv_categories").addEventListener("change",
       function(event) {
     onCategoryChange(elements, event);
@@ -119,7 +119,7 @@ function initializeOptions(elements, optionsObject, defaultOptions) {
 
 function saveOptions(elements, optionsObject) {
   readOptionsPage(elements, optionsObject);
-  
+
   if(optionsObject) {
     chrome.storage.sync.set(optionsObject, function() {
       if(!chrome.runtime.lastError) {
@@ -127,6 +127,7 @@ function saveOptions(elements, optionsObject) {
         status.textContent = "Options saved.";
         setTimeout(function() {
           status.textContent = "";
+          window.close();
         }, 2000);
       }
     });
@@ -142,12 +143,12 @@ function loadOptions(elements, optionsObject, defaultOptions) {
           optionsObject[prop] = items[prop];          
         }
       }
-    else {
-      console.log(chrome.runtime.lastError);
-      for(var prop in defaultOptions) {
-        optionsObject[prop] = defaultOptions[prop];
+      else {
+        console.log(chrome.runtime.lastError);
+        for(var prop in defaultOptions) {
+          optionsObject[prop] = defaultOptions[prop];
+        }
       }
-    }
     writeOptionsPage(elements, optionsObject);
   })
 }
@@ -157,7 +158,7 @@ function onCategoryChange(elements, event) {
     if(event.target.id == "all_movies")
       setAllCheckboxes(elements, true, event.target.checked);
     else
-     elements["all_movies"].checked = false;
+      elements["all_movies"].checked = false;
   }
   else if(event.target.name == "tv_category") {
     if(event.target.id == "all_tv")
